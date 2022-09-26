e89e2e623289d16b168071bb2b7f9d528a4794a0
Ubuntu <ubuntu@ip-172-31-4-215.eu-west-1.compute.internal>
2022-09-26 16:24:24 +0000

    cambio esquema

--- ejercicios/script2.js
+++ ejercicios/script2.js
@@ -23,7 +23,8 @@ db.createCollection("personas",{
             required: [
                 "nombre",
                 "edad",
-                "email"
+                "email",
+                "edad hermano mayor"
             ],
             properties: {
                 nombre: {
@@ -44,14 +45,32 @@ db.createCollection("personas",{
                     description: "El email es obligatorio y debe ser válido"
                 },
                 apellidos: {
+                    bsonType: "string"
                 },
                 "fecha nacimiento": {
                     bsonType: "date"
                 },
+                "edad hermano mayor": {
+                    bsonType: "int",
+                    minimum: 0,
+                    maximum: 150,
+                    description: "Campo requerido indicando la edad del hermano mayor del usuario, entre 0 y 150, incluidos"
+                }
             }
         }
     }
 })
+// Hemos creado una colección implicitamente.... Al usarla... pero ésto no nos da opción de configurar.
+db.personas.insertOne(
+{
+    "nombre": "Pepin",
+    "apellidos": 2,
+    "edad": 34,
+    "fecha nacimiento": ISODate("1978-08-27"),
+    "email": "ivan@ivan.com",
+    "edad hermano mayor": 40
+}
+);
 
 // Edades: "32" x 2 bytes UTF-8
 //          32  x 4 bytes
@@ -60,10 +79,52 @@ db.createCollection("personas",{
 db.runCommand( 
 { 
     collMod: "personas" ,
-    "validator": {/*Esquema de validación*/}
+    "validator": {
+        $jsonSchema: {
+            bsonType: "object", // Un documento
+            required: [
+                "nombre",
+                "edad",
+                "email",
+                "edad hermano mayor"
+            ],
+            properties: {
+                nombre: {
+                    bsonType: "string",
+                    description: "Campo requerido indicando el nombre del usuario",
+                    // Validaciones
+                    minLength: 2        // maxLength
+                },
+                edad: {
+                    bsonType: "int", // 32 bits = 4 bytes: Algo más de 4 mil millones 2^32
+                    minimum: 0,
+                    maximum: 150,
+                    description: "Campo requerido indicando la edad del usuario, entre 0 y 150, incluidos"
+                },
+                email: {
+                    bsonType: "string",
+                    pattern: "^[A-Za-z0-9._-]+@[A-Za-z0-9._-]+[.][A-Za-z0-9]{0,5}$",
+                    description: "El email es obligatorio y debe ser válido"
+                },
+                apellidos: {
+                    bsonType: "string"
+                },
+                "fecha nacimiento": {
+                    bsonType: "date"
+                },
+                "edad hermano mayor": {
+                    bsonType: "int",
+                    minimum: 0,
+                    maximum: 150,
+                    description: "Campo requerido indicando la edad del hermano mayor del usuario, entre 0 y 150, incluidos"
+                }
+            }
+        }
+    }
 })
 
 // OJO !!!!:
 
 // Que pasaría si modifico un esquema en una colección?
 // Lo que no cumpla fallará... Lo nuevo seguro... y lo que ya esté? No se ele aplica el esquema
+
